7069544e47f820442765142fbe7d3a42743a032c
Chinzorigo <chinzorig.o@gmail.com>
2023-04-09 02:16:57 +0800

    Movie API, Movie List and Card components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'upload.wikimedia.org',
+    //     port: '',
+    //     pathname: '/wikipedia/commons/7/70/**',
+    //   },
+    // ],
+    domains: [
+      'upload.wikimedia.org', 
+      'uhdtv.io', 
+      'mango.blender.org', 
+      'download.blender.org']
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { NextPageContext } from 'next'
 
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,6 +27,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
 
+  const {data: movies = []} = useMovieList();
 
   return (
     <>
@@ -37,6 +40,9 @@ export default function Home() {
       <main>
         <Navbar />
         <Billboard />
+        <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies}/>
+        </div>
       </main>
     </>
   )
